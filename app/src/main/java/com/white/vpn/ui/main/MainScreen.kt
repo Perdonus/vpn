4e1d5d7af56579d63551fcc836ab26b165326a53
Perdonus <kala5ixa@gmail.com>
2026-04-01 15:00:03 +0300

    Remove invalid Compose scope imports

--- app/src/main/java/com/white/vpn/ui/main/MainScreen.kt
+++ app/src/main/java/com/white/vpn/ui/main/MainScreen.kt
@@ -20,10 +20,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope.matchParentSize
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
